080af420a02109f00a28ad3d69ce0fb8fd8f74e5
madisonjbrooks12 <ec2-user@ip-172-31-31-150.us-west-2.compute.internal>
2015-03-15 01:08:59 +0000

    First Commit

--- static.js
+++ static.js
@@ -4,7 +4,48 @@ var url = require('url');
 var ROOT_DIR = "html/";
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
-  if(urlObj.pathname.indexOf("getcity") != -1) {
+  if(urlObj.pathname.indexOf("comment") !=-1) {
+    console.log("comment route");
+    if(req.method === "POST") {
+      console.log("POST comment route");
+      var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+        console.log("Name: "+reqObj.Name);
+        console.log("Comment: "+reqObj.Comment);
+        var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+          if(err) throw err;
+          db.collection('comments').insert(reqObj,function(err, records) {
+            console.log("Record added as "+records[0]._id);
+          });
+        });
+      });
+      res.writeHead(200);
+      res.end("");
+    } else if (req.method === "GET") {
+      console.log("In GET");
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+        if(err) throw err;
+        db.collection("comments", function(err, comments){
+          if(err) throw err;
+          comments.find(function(err, items){
+            items.toArray(function(err, itemArr){
+              console.log("Document Array: ");
+              console.log(itemArr);
+              res.writeHead(200);
+              res.end(JSON.stringify(itemArr));
+            });
+          });
+        });
+      });
+    }
+  } else if(urlObj.pathname.indexOf("getcity") != -1) {
     console.log("In Getcity");
     fs.readFile("cities.dat.txt", function (err,data) {
       if (err) throw err;
